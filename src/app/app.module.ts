45c4097452fcea7bc4d68fe2a6c83447cbaf6525
Fernando Ocampo <fernando.ocampo@ravencall.io>
2017-11-24 00:55:52 -0500

    added skeleton of the GUI for searching users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MatchesComponent } from './matches/matches.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchesComponent
   ],
   imports: [
     BrowserModule
