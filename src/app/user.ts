6490b76f6300e12dc4e50e3f8b2b43350b24ef5a
Fernando Ocampo <fernando.ocampo@ravencall.io>
2017-11-24 00:41:01 -0500

    Added user model to show in the result

---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    displayname: string;
+    age: number;
+    height: number;
+    jobtitle: string;
+    mainphoto: string;
+    compatabilityScore: number;
+    contactsExchanged: number;
+    religion: string;
+    cityname: string;
+    favourite: boolean;
+}
